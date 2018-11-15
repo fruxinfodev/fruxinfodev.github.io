3e73f2f40647fffe66ee440e7f44dd2a13049869
fruxinfodev <35678870+fruxinfodev@users.noreply.github.com>
2018-11-15 16:27:22 +0530

    added "/dwellerin"

---
+++ dwellerin/js/myjs.js
@@ -0,0 +1,113 @@
+$('.responsive').slick({
+  dots: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  infinite: true,
+   autoplay:true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.image').slick({
+  dots: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  infinite: true,
+   autoplay:true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+  $('#datepicker').datepicker({
+            uiLibrary: 'bootstrap4'
+        });
+
+  $(window).load(function() {
+    $("#flexiselDemo3").flexisel({
+        visibleItems: 2,
+        itemsToScroll: 1,         
+        autoPlay: {
+            enable: true,
+            interval: 2000,
+            pauseOnHover: true
+        },
+        responsiveBreakpoints: { 
+            portrait: { 
+                changePoint:480,
+                visibleItems: 1,
+                itemsToScroll: 1
+            }, 
+            landscape: { 
+                changePoint:640,
+                visibleItems: 2,
+                itemsToScroll: 2
+            },
+            tablet: { 
+                changePoint:768,
+                visibleItems: 3,
+                itemsToScroll: 3
+            }
+        }
+    });
+});
+
+              $('#bs4-multi-slide-carousel').carousel({
+  interval: 1500
+})
