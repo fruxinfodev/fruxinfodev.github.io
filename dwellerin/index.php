3d835bb23fa8c03ab003149931596260f7c53f48
fruxinfodev <35678870+fruxinfodev@users.noreply.github.com>
2018-11-15 16:49:57 +0530

    Update index.php

--- dwellerin/index.php
+++ dwellerin/index.php
@@ -1,4 +1,56 @@
-﻿<?php include 'header.php' ?>
+﻿<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Dwellerin</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/font-awesome.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="css/bootstrap-datepicker.css" rel="stylesheet">
+         <link href="css/slick.min.css" rel="stylesheet">
+        <link href="css/custome.css" rel="stylesheet">
+        <link href="css/responsive.css" rel="stylesheet">
+        <link href="css/animate.min.css" rel="stylesheet">
+        <link href="img/favicon.png" rel="shortcut icon" />
+        <script src="js/jquery-1.11.1.js"></script>
+        <script src="js/bootstrap-datepicker.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/slick.min.js"></script>
+        <script src="js/jquery.flexisel.js"></script>
+        <script src="js/myjs.js"></script>
+    </head>
+    <body>
+          
+        <section class="header">
+                <nav class="navbar navbar-expand-md">
+                  <a class="navbar-nav mr-auto navbar-logo logo d-none" href="#">Dwellerin</a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <a class="navbar-nav mr-auto sp_logo logo" href="#">Dwellerin</a>
+                    <ul class="navbar-nav">                      
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">List Your Property</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Create Account | Login</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Help</a>
+                      </li>
+                      <li class="nav-item right-zero">
+                        <a class="nav-link request" href="#">+91 8449346393</a>
+                      </li>    
+                    </ul>
+                  </div>  
+                </nav>
+        </section>
 
         <section class="slider section-padding" >
             <div class="container">
@@ -482,7 +534,136 @@
             </div>
             </div>
             </div>
+        </section><section class="footer">
+            <div class="row d-flex justify-content-center border-bottom">
+                <div class="col-lg-10 col-md-12">
+                    <div class="row mt-3 mb-3">
+                        <div class="col-md-3 col-sm-6 col-6 mt-5">
+                            <button class="btn my-btn">+91 8449346393</button>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-12 b-right border-left mt-2 text-left m-zero">
+                            <ul class="m-zero ml-3">
+                                <li>
+                                   <a href="#"> About us</a>
+                                </li>
+                                <li>
+                                   <a href="#"> Team / Careers</a>
+                                </li>
+                                <li>
+                                   <a href="#"> Blog</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-12 b-right border-right mt-2 text-left m-zero">
+                            <ul class="m-zero">
+                                <li>
+                                   <a href="#"> Support</a>
+                                </li>
+                                <li>
+                                   <a href="#"> Official Blog</a>
+                                </li>
+                                <li>
+                                   <a href="#"> Press Kit</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-12 mt-2 b-right text-left m-zero">
+                            <ul class="m-zero">
+                                <li>
+                                   <a href="#"> Privacy Policy</a>
+                                </li>
+                                <li>
+                                   <a href="#"> Guest Policy</a>
+                                </li>
+                                <li>
+                                   <a href="#">terms & Condition</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           <!--  <div class="row d-flex justify-content-center border-bottom">
+                <div class="col-md-10">
+                    <div class="row mt-3 mb-3">
+                        <div class="col-md-6 b-right border-right">                            
+                            <p class="mt-3">Found security issues, that you’d like us to know?<a href="#"><b>Submit an issue</b></a></p>
+                        </div>
+                        <div class="col-md-6">                            
+                            <p class="mt-3">Have Queries ? Call for any assistance on <b>+91 931 393 1393</b></p>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+            <div class="row d-flex justify-content-center border-bottom">
+                <div class="col-md-10">
+                    <div class="row mt-3 mb-3">
+                        <div class="col-md-5 text-right">                            
+                            <h6 class="mt-3">Subscribe for exciting deals & offer</h6>
+                        </div>
+                        <div class="col-md-5">
+                            <form>
+                              <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="">
+                                <div class="input-group-append footer-search  ">
+                                  <button class="btn" type="submit"><i class="fa fa-search"></i></button> 
+                                </div>
+                              </div>
+
+
+                              <!-- <div class="input-group mb-0">
+                                <input type="text" class="form-control" placeholder="">
+                                <div class="input-group-btn footer-search">
+                                  <button class="btn btn-default" type="submit">
+                                    <i class="fa fa-search"></i>
+                                  </button>
+                                </div>
+                              </div> -->
+                            </form>
+                        </div>                       
+                    </div>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center border-bottom">
+                <div class="col-md-10">
+                    <div class="row mt-3 mb-3">
+                        <div class="col-md-12 text-center">
+                            <h5 style="color: #7e7d7d">Partners</h5>
+                            <p class="mt-3 mb-0">
+                              <a href="#">Partner</a> / 
+                              <a href="#">Partner</a> / 
+                              <a href="#">Partner</a> / 
+                              <a href="#">Partner</a> / 
+                              <a href="#">Partner</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-10">
+                    <div class="row mt-3 mb-3">
+                        <div class="col-md-3 col-sm-3 col-12">
+                           <a href="#"><img src="img/insta.png" class="img-fluid mt-3 mr-1"></a>
+                           <a href="#"><img src="img/fb.png" class="img-fluid mt-3 mr-1"></a>
+                           <a href="#"><img src="img/twitter.png" class="img-fluid mt-3 mr-1"></a>
+                           <a href="#"><img src="img/linkedin.png" class="img-fluid mt-3 mr-1"></a>
+                           <a href="#"><img src="img/app.png" class="img-fluid mt-3 mr-1"></a>
+                        </div>
+                        <div class="col-md-6 col-sm-3 col-12 text-center">
+                            <a class="logo" href="#">Dwellerin</a>
+                        </div>
+                        <div class="col-md-3 col-sm-3 col-12">
+                           <p class="pt-3 text-right">© 2018  <a href="#"> igloo </a>- All rights reserved</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
-        
-<?php include 'footer.php' ?>
+        <script src="js/wow.min.js"></script>
+        <script src="js/myjs.js"></script>
+        <script type="text/javascript" src="js/jquery.flexisel.js"></script>
+       
+    </body>
+</html>
